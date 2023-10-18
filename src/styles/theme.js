f90d60dfe3635b86a2f6b284c2b63ce6736bf11e
Bruno S Velho <bruno.velho@gmail.com>
2023-10-18 10:23:15 -0300

    Init Project

---
+++ src/styles/theme.js
@@ -0,0 +1,22 @@
+export default {
+  COLORS: {
+
+    BLACK: "#000000",
+    WHITE: "#F4EDE8",
+    ORANGE: "#FF9000",
+    PINK: "#FF859B",
+    RED: "#FF002E",
+
+    GRAY_900: "#1C1B1E",
+    GRAY_800: "#232129",
+    GRAY_700: "#312E38",
+    GRAY_600: "#3E3B47",
+    GRAY_500: "#666360",
+    GRAY_400: "#999591",
+    GRAY_300: "#948F99",
+    GRAY_200: "#CAC4CF",
+    GRAY_100: "#E5E5E5",
+    GRAY_000: "#F4EDE8",
+
+  }
+}
